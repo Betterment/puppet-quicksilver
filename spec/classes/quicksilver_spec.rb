17fb5f7be3130fe1adafe35e714e4987f0a41d9b
Alan Norton <me@alannorton.com>
2013-10-16 21:18:57 -0400

    fixed test url

--- spec/classes/quicksilver_spec.rb
+++ spec/classes/quicksilver_spec.rb
@@ -4,6 +4,6 @@ describe 'quicksilver' do
 
   it { should contain_class('quicksilver') }
   it { should contain_package('Quicksilver').with_provider('appdmg') }
-  it { should contain_package('Quicksilver').with_source('http://cdn.qsapp.com/com.blacktree.Quicksilver__16388.dmg') }
+  it { should contain_package('Quicksilver').with_source('http://github.qsapp.com/downloads/Quicksilver%201.1.2.dmg') }
 
 end
